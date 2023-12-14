d83ac74326d654decf5f749daeb4a2a35e193cbe
ajit51 <ajitk433@gmail.com>
2023-12-14 09:52:14 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to java pipeline example");
+        System.out.println(new Date());
+        System.out.println("jenkins pipeline using Dockerfile");
+    }
+}
